fb38cd091904a56c0676206c064755dbcdd62291
Fierroski <asferro18@ucatolica.edu.co>
2026-08-22 12:28:39 -0500

    romanos

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -96,6 +96,7 @@ class Conversion:
         pass
     
     def binario_a_decimal(self, binario):
+        return int(binario, 2)
         """
         Convierte un número binario a decimal.
         
@@ -111,7 +112,20 @@ class Conversion:
         """
         pass
     
-    def decimal_a_romano(self, numero):
+    def decimal_a_romano(self, numero: int):
+        valores = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'),
+            (400, 'CD'), (100, 'C'), (90, 'XC'),
+            (50, 'L'), (40, 'XL'), (10, 'X'),
+            (9, 'IX'), (5, 'V'), (4, 'IV'), 
+            (1, 'I')
+            ]
+        resultado = ""
+        for valor, simbolo in valores:
+            while numero >= valor:
+                resultado += simbolo
+                numero -= valor
+        return resultado
         """
         Convierte un número decimal a numeración romana.
         
@@ -128,6 +142,21 @@ class Conversion:
         pass
     
     def romano_a_decimal(self, romano):
+        romanos = {
+            'I': 1, 'V': 5,
+            'X': 10, 'L': 50, 'C': 100,
+            'D': 500, 'M': 1000
+            }
+        total = 0
+        prev_value = 0
+        for letra in reversed(romano):
+            valor = romanos[letra]
+            if valor < prev_value:
+                total -= valor
+            else:
+                total += valor
+            prev_value = valor
+        return total
         """
         Convierte un número romano a decimal.
         
